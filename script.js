13bddf868e50db98a99e541e67bb1bf1dd4bfb8d
kirankumar2007 <200703kirankumar@gmail.com>
2024-08-17 09:06:51 +0000

    gave more gradient and improved functionality

--- script.js
+++ script.js
@@ -65,89 +65,80 @@ const quizData = [
     }
 ];
 
-const quiz = document.getElementById("quiz");
-const questionEl = document.getElementById("question");
-const currentQuestionEl = document.getElementById("current-question");
-const totalQuestionsEl = document.getElementById("total-questions");
-const answerEls = document.querySelectorAll(".answer");
-const a_text = document.getElementById("a_text");
-const b_text = document.getElementById("b_text");
-const c_text = document.getElementById("c_text");
-const d_text = document.getElementById("d_text");
-const submitBtn = document.getElementById("submit");
+
+const questionEl = document.getElementById('question');
+const optionsContainer = document.querySelector('.options');
+const submitBtn = document.getElementById('submit');
 
 let currentQuiz = 0;
 let score = 0;
-
-totalQuestionsEl.innerText = quizData.length;
+let selectedAnswer = undefined;
 
 loadQuiz();
 
 function loadQuiz() {
     deselectAnswers();
-    const currentQuizData = quizData[currentQuiz];
 
+    const currentQuizData = quizData[currentQuiz];
     questionEl.innerText = currentQuizData.question;
-    a_text.innerText = currentQuizData.a;
-    b_text.innerText = currentQuizData.b;
-    c_text.innerText = currentQuizData.c;
-    d_text.innerText = currentQuizData.d;
-    currentQuestionEl.innerText = currentQuiz + 1;
+
+    optionsContainer.innerHTML = `
+        <label>
+            <input type="radio" name="answer" value="a">
+            ${currentQuizData.a}
+        </label>
+        <label>
+            <input type="radio" name="answer" value="b">
+            ${currentQuizData.b}
+        </label>
+        <label>
+            <input type="radio" name="answer" value="c">
+            ${currentQuizData.c}
+        </label>
+        <label>
+            <input type="radio" name="answer" value="d">
+            ${currentQuizData.d}
+        </label>
+    `;
+
+    document.querySelectorAll('.options label').forEach(option => {
+        option.addEventListener('click', () => {
+            deselectAnswers();
+            option.classList.add('selected');
+            selectedAnswer = option.querySelector('input').value;
+            submitBtn.disabled = false;
+        });
+    });
 }
 
 function deselectAnswers() {
-    answerEls.forEach(answerEl => {
-        answerEl.checked = false;
-        answerEl.parentElement.classList.remove("selected", "correct", "incorrect");
+    document.querySelectorAll('.options label').forEach(option => {
+        option.classList.remove('selected');
     });
+    submitBtn.disabled = true;
 }
 
-function getSelected() {
-    let answer;
-    answerEls.forEach(answerEl => {
-        if(answerEl.checked) {
-            answer = answerEl.id;
-        }
-    });
-    return answer;
-}
-
-answerEls.forEach(answerEl => {
-    answerEl.addEventListener("change", () => {
-        deselectAnswers();
-        answerEl.parentElement.classList.add("selected");
-    });
-});
-
-submitBtn.addEventListener("click", () => {
-    const answer = getSelected();
-    
-    if(answer) {
-        const correctAnswer = quizData[currentQuiz].correct;
-
-        if(answer === correctAnswer) {
+submitBtn.addEventListener('click', () => {
+    if (selectedAnswer) {
+        if (selectedAnswer === quizData[currentQuiz].correct) {
             score++;
         }
 
-        answerEls.forEach(answerEl => {
-            if(answerEl.id === correctAnswer) {
-                answerEl.parentElement.classList.add("correct");
-            } else if(answerEl.checked) {
-                answerEl.parentElement.classList.add("incorrect");
-            }
-        });
-
         currentQuiz++;
 
-        setTimeout(() => {
-            if(currentQuiz < quizData.length) {
-                loadQuiz();
-            } else {
-                quiz.innerHTML = `
-                    <h2>You answered ${score}/${quizData.length} questions correctly</h2>
-                    <button onclick="location.reload()">Reload</button>
-                `;
-            }
-        }, 1000); // Add a delay before loading the next question or showing the result
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            showResults();
+        }
     }
 });
+
+function showResults() {
+    optionsContainer.innerHTML = `
+        <h2>You answered ${score}/${quizData.length} questions correctly.</h2>
+        <button onclick="location.reload()">Reload</button>
+    `;
+    questionEl.innerHTML = '';
+    submitBtn.style.display = 'none';
+}
